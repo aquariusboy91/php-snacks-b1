4da4e98a802ad6b198a3aa8833d49902fcc8cefe
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-04 17:26:19 +0100

    add style

--- index-php.php
+++ index-php.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>Php Snacks b1 </title>
 </head>
 <body>
     <main class="main-cont" id="app">
         <div class="img-cont">
             <div class="cards" v-for="card in cars">
-                    <h2>{{card.casa}} {{card.modello}}</h3>
+                    <h1>{{card.casa}} {{card.modello}}</h1>
                     <ul>
                         <li>Tipo: {{card.tipo}}</li>
                         <li>Prezzo: {{card.prezzo}}</li>
